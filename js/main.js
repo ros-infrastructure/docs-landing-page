d2459b04ed4484e3cc8b55625658347d17b2042d
kscottz <katherineAScott@gmail.com>
2025-02-10 14:29:11 -0800

    Add a ROS Index search bar.

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const rosDistributionsDiv = document.querySelector('#distributions');
   renderDistributionsCards(rosDistributionsDiv, rosDistributions);
 });
+
+// Add ROS Index search capability
+document.getElementById('search-index').onclick=function(){
+  var value = document.getElementById('search').value;
+  window.open('https://index.ros.org/?pkgs='+encodeURIComponent(value)+"&search_packages=true", '_blank')
+}
