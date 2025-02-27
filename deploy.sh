42078053cc4e9f299d57251d62c2d7c378ea54f5
Steven! Ragnarök <steven@nuclearsandwich.com>
2025-02-26 16:52:31 -0800

    Add build and deploy scripts.

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+DOCS_USER=${DOCS_USER:-rosdocs}
+DOCS_HOST=${DOCS_HOST:-docs.ros.org}
+DOCS_PATH=${DOCS_PATH:-/var/www/docs.ros.org}
+DEPLOY_ARCHIVE=${DEPLOY_ARCHIVE:-docs-landing-page.tar.gz}
+
+scp ${SSH_ARGS} ${DOCS_USER}@${DOCS_HOST}:${DOCS_PATH}
+ssh -T ${SSH_ARGS} ${DOCS_USER}@${DOCS_HOST} tar -C ${DOCS_PATH} -zxf ./${DEPLOY_ARCHIVE}
